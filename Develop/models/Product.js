758f4c1976e0fdd059108ad868aa06987fca7345
Robert Campagna <rwcnyla@gmail.com>
2021-01-25 17:53:57 -0800

    added models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, DECIMAL } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,8 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
   },
+    price: {
+    type: DECIMAL(10,2),
+    allowNull: false,
+    validate: {
+      isDECIMAL: true,
+    },
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+      validate: {
+        isNumeric: true,
+    },
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: { 
+      model: 'category',
+      key: 'id'
+    },
+  },
+     // define columns
+  },
+
   {
     sequelize,
     timestamps: false,
